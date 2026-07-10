b1995c01a516cdae0dde920cab88429b943d4d21
Subuzy <bestsnowkicker@gmail.com>
2026-07-10 12:29:54 -0400

    removed an unnecessary part and fixed braces.

--- Factor.java
+++ Factor.java
@@ -1,42 +1,17 @@
-public class Factor
-{
-	public static void main(String[] args)
-	{
-		for(byte t = 0; t < 2; ++t)
-		{
-			long N = Long.parseLong(args[0]);
-			long n = N;
-			for (long i = 2; i <= n/i; i++)
-			{
-				while (n % i == 0)
-				{
-					n /= i;
-					System.out.print(i + " ");
-				}
-			}
-			if (n > 1) System.out.print(n);
-			System.out.println();
-		//my vers vs book vers
-
-	
-			n = N;
-			while (n % 2 == 0)
-			{
-				n /= 2;
-				System.out.print("2 ");
-			}
-			for (long i = 3; i <= n/i; i += 2)
-			{
-				while (n % i == 0)
-				{
-					n /= i;
-					System.out.print(i + " ");
-				}
-			}
-			if (n > 1) System.out.print(n);
-			System.out.println();
+public class Factor {
+	public static void main(String[] args) {
+		long N = Long.parseLong(args[0]);
+		while (n % 2 == 0) {
+			n /= 2;
+			System.out.print("2 ");
 		}
-
-
+		for (long i = 3; i <= n/i; i += 2) {
+			while (n % i == 0) {
+				n /= i;
+				System.out.print(i + " ");
+			}
+		}
+		if (n > 1) System.out.print(n);
+		System.out.println();
 	}
 }
