a53ae4027352f6d7aa0e83298774a16f166e9da6
Subuzy <bestsnowkicker@gmail.com>
2026-08-02 14:46:02 -0400

    made a separate branch for unit interval, and put others on another branch

--- IsUnitInterval.java
+++ IsUnitInterval.java
@@ -1,8 +1,9 @@
-public class Between0and1 {
+public class IsUnitInterval {
+	//prints true if both arguments are unit intervals, and false if not
 	public static void main(String[] args) {
 		Double a = Double.parseDouble(args[0]);
 		Double b = Double.parseDouble(args[1]);
-		//check and print
+		check and print
 		if (a >= 0 && a <= 1 && b >= 0 && b <= 1) {
 			System.out.println("true");
 		} else {
